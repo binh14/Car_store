c9947b99dd016deac1acf954e8b8eb9429aaf7c4
Van Binh <vanbinhpham202@gmail.com>
2024-01-24 10:46:03 +0700

    add code

---
+++ api/car/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from car.models import Car
+
+class CarSerializer(serializers.ModelSerializer):
+
+    class Meta: 
+        model = Car
+        fields = ('id', 'name', 'color', 'brand')
+    
+    def test():
+        return "a"
